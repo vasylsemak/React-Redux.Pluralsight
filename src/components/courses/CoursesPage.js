42e09226472a9ec9c13223be0df7f4cf2a3d06b4
vasylsemak <vasylsmk1401@gmial.com>
2022-05-27 10:56:31 -0700

    refactor CoursesPage: add loadAthors to mapDispatch, dispatch it when component mount

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,15 +4,25 @@ import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import CourseList from './CourseList'
 import * as courseActions from '../../redux/actions/courseActions'
+import * as authorActions from '../../redux/actions/authorActions'
 
 class CoursesPage extends React.Component {
   componentDidMount() {
-    this.props.actions
-      .loadCourses()
-      .catch((err) => alert('Loading courses has failed! ' + err))
+    const { courses, authors, loadCourses, loadAuthors } = this.props
+
+    if (courses.length === 0) {
+      loadCourses().catch((err) => alert('Loading Courses has failed! ' + err))
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((err) => alert('Loading Authors has failed! ' + err))
+    }
   }
 
   render() {
+    const { courses, authors } = this.props
+    console.log('courses: ', courses)
+    console.log('authors: ', authors)
+
     return (
       <>
         <h2>Courses</h2>
@@ -24,14 +34,19 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = (state) => ({
   courses: state.courses,
+  authors: state.authors,
 })
+
 const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(courseActions, dispatch),
+  loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+  loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
