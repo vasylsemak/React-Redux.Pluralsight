7936fba02d55d75cf5d3892d6a79f80b07f26bf4
vasylsemak <vasylsmk1401@gmial.com>
2022-06-23 09:26:25 -0700

    create courseActions.test.js

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -3,12 +3,13 @@ import Header from './Header'
 import { mount, shallow } from 'enzyme'
 import { MemoryRouter } from 'react-router-dom'
 
-// Note how with shallow render you search for the React component tag
+// shallow randering is fast, looks through JSX, tests component in isolation to children
 it('contains 3 NavLinks via shallow', () => {
   const numLinks = shallow(<Header />).find('NavLink').length
   expect(numLinks).toEqual(3)
 })
 
+// mount() - when you test final DOM or interaction with child components
 // Note how with mount you search for the final rendered HTML since it generates the final DOM.
 // We also need to pull in React Router's memoryRouter for testing since the Header expects to have React Router's props passed in.
 it('contains 3 anchors via mount', () => {
--- src/redux/actions/courseActions.test.js
+++ src/redux/actions/courseActions.test.js
@@ -1,6 +1,32 @@
 import * as courseActions from './courseActions'
 import * as actionTypes from './actionTypes'
 import { courses } from '../../../tools/mockData'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+import configureMockStore from 'redux-mock-store'
+
+// test an async action
+const middleware = [thunk]
+const mockStore = configureMockStore(middleware)
+
+describe('Async Actions', () => {
+  afterEach(() => {
+    // to mock fetch call after each thunk
+    fetchMock.restore()
+  })
+
+  describe('Load Courses Thunk', () => {
+    it('should create BE', () => {
+      fetchMock.mock('*', {
+        // api call responce mock
+        // capture any fetch calls, return body with array of courses
+        // with a header set to application/json
+        body: courses,
+        headers: { 'content-type': 'application/json' },
+      })
+    })
+  })
+})
 
 describe('createCourseSuccess', () => {
   it('should create a CREATE_COURSE_SUCCESS action', () => {
