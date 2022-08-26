ee834a9c8cbf14c31f36cc9fc69f5f0efd6d36ef
Vasyl Semak <vasylsmk1401@gmail.com>
2022-08-26 17:13:38 -0400

    refactor return in CoursesForm.js

--- src/components/courses/CourseForm.js
+++ src/components/courses/CourseForm.js
@@ -10,50 +10,48 @@ const CourseForm = ({
   onChange,
   saving,
   errors = {},
-}) => {
-  return (
-    <form onSubmit={onSave}>
-      <h2>{course.id ? 'Edit' : 'Add'} Course</h2>
-      {errors.onSave && (
-        <div className='alert alert-danger' role='alert'>
-          {errors.onSave}
-        </div>
-      )}
-      <TextInput
-        name='title'
-        label='Title'
-        value={course.title}
-        onChange={onChange}
-        error={errors.title}
-      />
+}) => (
+  <form onSubmit={onSave}>
+    <h2>{course.id ? 'Edit' : 'Add'} Course</h2>
+    {errors.onSave && (
+      <div className='alert alert-danger' role='alert'>
+        {errors.onSave}
+      </div>
+    )}
+    <TextInput
+      name='title'
+      label='Title'
+      value={course.title}
+      onChange={onChange}
+      error={errors.title}
+    />
 
-      <SelectInput
-        name='authorId'
-        label='Author'
-        value={course.authorId || ''}
-        defaultOption='Select Author'
-        options={authors.map((author) => ({
-          value: author.id,
-          text: author.name,
-        }))}
-        onChange={onChange}
-        error={errors.author}
-      />
+    <SelectInput
+      name='authorId'
+      label='Author'
+      value={course.authorId || ''}
+      defaultOption='Select Author'
+      options={authors.map((author) => ({
+        value: author.id,
+        text: author.name,
+      }))}
+      onChange={onChange}
+      error={errors.author}
+    />
 
-      <TextInput
-        name='category'
-        label='Category'
-        value={course.category}
-        onChange={onChange}
-        error={errors.category}
-      />
+    <TextInput
+      name='category'
+      label='Category'
+      value={course.category}
+      onChange={onChange}
+      error={errors.category}
+    />
 
-      <button type='submit' disabled={saving} className='btn btn-primary'>
-        {saving ? 'Saving...' : 'Save'}
-      </button>
-    </form>
-  )
-}
+    <button type='submit' disabled={saving} className='btn btn-primary'>
+      {saving ? 'Saving...' : 'Save'}
+    </button>
+  </form>
+)
 
 CourseForm.propTypes = {
   authors: PropTypes.array.isRequired,
